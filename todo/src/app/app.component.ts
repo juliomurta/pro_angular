83983956b42e5fc9512e5af1bf8ce65acf397b74
Julio Cesar Souza Murta <souzamurtaj@gmail.com>
2024-09-30 09:05:14 -0300

    create todo app

---
+++ todo/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { TodoList } from './todolist';
+import { TodoItem } from './todoItem';
+import { NgForOf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    NgSwitch, 
+    NgSwitchCase, 
+    NgSwitchDefault,
+    NgForOf,
+    FormsModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  private list = new TodoList("Bob", [
+    new TodoItem("Go for run", true),
+    new TodoItem("Get flowers"),
+    new TodoItem("Collect tickets")
+  ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items;
+  }
+}
