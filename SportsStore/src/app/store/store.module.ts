c4b46aa78ba911536ec1981dad67d1ea95334cab
Julio Cesar Souza Murta <souzamurtaj@gmail.com>
2024-11-04 08:49:14 -0300

    create detail and checkout components

--- SportsStore/src/app/store/store.module.ts
+++ SportsStore/src/app/store/store.module.ts
@@ -5,10 +5,22 @@ import { StoreComponent } from "./store.component";
 import { CommonModule, CurrencyPipe } from "@angular/common";
 import { CounterDirective } from "./counter.directive";
 import { CartSummaryComponent } from "./cartSummary.component";
+import { CartDetailComponent } from "./cartDetail.component";
+import { CheckoutComponent } from "./checkout.component";
 
 @NgModule({
-    imports: [ModelModule, FormsModule, CommonModule],
-    declarations: [StoreComponent, CounterDirective, CartSummaryComponent],
+    imports: [
+        ModelModule, 
+        FormsModule, 
+        CommonModule
+    ],
+    declarations: [
+        StoreComponent, 
+        CounterDirective, 
+        CartSummaryComponent, 
+        CartDetailComponent, 
+        CheckoutComponent
+    ],
     exports: [StoreComponent]
 })
 export class StoreModule { }
