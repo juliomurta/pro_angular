3999d96b85acd06274b731497f39cb3aa92facb6
Julio Cesar Souza Murta <souzamurtaj@gmail.com>
2024-11-12 08:38:38 -0300

    Implement rest datasource

--- SportsStore/src/app/model/model.module.ts
+++ SportsStore/src/app/model/model.module.ts
@@ -4,14 +4,20 @@ import { StaticDataSource } from "./static.datasource";
 import { Cart } from "./cart.model";
 import { OrderRepository } from "./order.repository";
 import { Order } from "./order.model";
+import { RestDataSource } from "./rest.datasource";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
+    imports: [HttpClientModule],
     providers: [   
         ProductRepository, 
         OrderRepository,
-        StaticDataSource, 
         Cart,
-        Order
+        Order,
+        {
+            provide: StaticDataSource,
+            useClass: RestDataSource
+        }
     ]
 })
 export class ModelModule {}
