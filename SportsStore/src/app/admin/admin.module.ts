86df3a0cce4aa28b1c91687df490253cc76f65c6
Julio Cesar Souza Murta <souzamurtaj@gmail.com>
2024-11-19 09:14:42 -0300

    finish product and order modules

--- SportsStore/src/app/admin/admin.module.ts
+++ SportsStore/src/app/admin/admin.module.ts
@@ -6,6 +6,9 @@ import { AdminComponent } from "./admin.component";
 import { RouterModule } from "@angular/router";
 import { AuthGuard } from "./auth.guard";
 import { ModelModule } from "../model/model.module";
+import { OrderTableComponent } from "./orderTable.component";
+import { ProductEditorComponent } from "./productEditor.component";
+import { ProductTableComponent } from "./productTable.component";
 
 let routing = RouterModule.forChild([
     {
@@ -15,7 +18,29 @@ let routing = RouterModule.forChild([
     {
         path: "main",
         component: AdminComponent,
-        canActivate: [AuthGuard]
+        canActivate: [AuthGuard],
+        children: [
+            { 
+                path: "products/:mode/:id", 
+                component: ProductEditorComponent 
+            },
+            { 
+                path: "products/:mode", 
+                component: ProductEditorComponent 
+            },
+            { 
+                path: "products", 
+                component: ProductTableComponent 
+            },
+            { 
+                path: "orders", 
+                component: OrderTableComponent 
+            },
+            { 
+                path: "**", 
+                redirectTo: "products" 
+            }
+        ]
     },
     {
         path: "**",
@@ -24,8 +49,19 @@ let routing = RouterModule.forChild([
 ]);
 
 @NgModule({
-    imports: [CommonModule, FormsModule,ModelModule, routing],
+    imports: [
+        CommonModule, 
+        FormsModule, 
+        ModelModule, 
+        routing
+    ],
     providers: [AuthGuard],
-    declarations: [AuthComponent, AdminComponent]
+    declarations: [
+        AuthComponent, 
+        AdminComponent, 
+        ProductTableComponent,
+        ProductEditorComponent,
+        OrderTableComponent
+    ]
 })
 export class AdminModule { }
